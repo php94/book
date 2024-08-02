e64c708d78c077ba194a50c0d2597fde8ded6e23
John Doe <johndoe@example.com>
2024-08-02 14:19:44 +0800

    1

--- src/library/Http/Home/File.php
+++ src/library/Http/Home/File.php
@@ -4,13 +4,31 @@ declare(strict_types=1);
 
 namespace App\Php94\Book\Http\Home;
 
-use App\Php94\Admin\Http\Common;
 use PHP94\Facade\Factory;
+use PHP94\Facade\Framework;
 use PHP94\Help\Request;
+use PHP94\Help\Response;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class File extends Common
+class File implements RequestHandlerInterface
 {
+    public function handle(
+        ServerRequestInterface $request
+    ): ResponseInterface {
+        $method = strtolower($request->getMethod());
+        if (in_array($method, ['get', 'put', 'post', 'delete', 'head', 'patch', 'options']) && is_callable([$this, $method])) {
+            $resp = Framework::execute([$this, $method]);
+            if (is_scalar($resp) || (is_object($resp) && method_exists($resp, '__toString'))) {
+                return Response::html((string)$resp);
+            }
+            return $resp;
+        } else {
+            return Response::error('不支持该请求');
+        }
+    }
+
     public function get(): ResponseInterface
     {
         switch (Request::get('file')) {
